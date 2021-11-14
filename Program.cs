0bb82ac2649a231aabc3d5b9965e381c33dfff57
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-14 20:20:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace typecasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 2;
+            double myDouble = 32.99;
+            bool myBool = false;
+            Console.WriteLine(Convert.ToString(myBool));
+            Console.WriteLine(Convert.ToInt32(myDouble));
+            Console.WriteLine(Convert.ToDouble(myInt));
+
+        }
+    }
+}
